8bfe07dcd899ecc85b6f6e3cc6e3c5cb5459fa32
tia-nn <yknms3533642@yahoo.co.jp>
2024-09-28 12:07:03 +0900

    ちょっとメモ

--- src/components/viewer/BuiltinViewer.tsx
+++ src/components/viewer/BuiltinViewer.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import './BuiltinViewer.css';
+import { encodeUTF16BEA } from '@/tools/encoding';
 
 const BuiltinViewer: React.FC<{ value: string }> = ({ value }) => {
     const [url, setUrl] = useState<string | null>();
 
     useEffect(() => {
-        const pdf = new Blob([value], { type: "application/pdf" });
+        const pdf = new Blob([encodeUTF16BEA(value)], { type: "application/pdf" });
         const url = URL.createObjectURL(pdf);
         setUrl(url);
         return () => URL.revokeObjectURL(url);
